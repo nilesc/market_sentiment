eba50dfae09381ae238d5a72f3cf96ea28d7de3a
Niles Christensen <nnc2117@columbia.edu>
2019-04-22 18:42:21 -0400

    [Changes] scraper to use a file of tickers

--- finance_scraper/scraper.py
+++ finance_scraper/scraper.py
@@ -1,8 +1,23 @@
+import sys
 from yahoo_quote_download.yahoo_quote_download import yqd
 
-ticker = ['QCOM', 'C', '^DJI']
+def get_data(tickers):
+    start = '20180101'
+    end = '20190101'
 
-start = '20190101'
-end = '20190127'
-for t in ticker:
-    print(yqd.load_yahoo_quote(t, start, end))
+    responses = []
+
+    for t in tickers:
+        cleaned = t[:-1]
+        responses.append(yqd.load_yahoo_quote(cleaned, start, end))
+
+    print(len(responses))
+    return responses
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Must provide a file to read tickers from")
+        sys.exit()
+
+    ticker_file = open(sys.argv[1], 'r')
+    print(get_data(ticker_file))
