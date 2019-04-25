17ef4dc5f1ee2fdcb3c333ba26a08f3701f6a770
Niles Christensen <nnc2117@columbia.edu>
2019-04-25 00:56:28 -0400

    [Adds] a working peak detector

--- peak_detector.py
+++ peak_detector.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import sys
+import statistics
 
 
 def get_volume(company_dir):
@@ -15,6 +16,19 @@ def get_volume(company_dir):
     return tweet_volume
 
 
+def get_peaks(volumes, window_width, threshold):
+    peaks = []
+
+    for i in range(len(volumes)-2*window_width):
+        day_index = i + window_width
+        window = volumes[i:day_index+window_width+1]
+        median = statistics.median(window)
+        if volumes[day_index] > threshold * median:
+            peaks.append(day_index)
+
+    return peaks
+
+
 if __name__ == '__main__':
     if len(sys.argv) is not 2:
         print('Requires a path to the tweet directory')
@@ -24,11 +38,17 @@ if __name__ == '__main__':
     all_companies = [company for company in os.listdir(tweets_directory)
                      if os.path.isdir(os.path.join(tweets_directory, company))]
 
+    window_width = 5
+    threshold = 2
+
     volumes = {}
 
     for company in all_companies:
+        print(f'Data for {company}')
         company_dir = os.path.join(tweets_directory, company)
         volumes[company] = get_volume(company_dir)
+        peaks = get_peaks(volumes[company], window_width, threshold)
+        print(peaks)
 
     print(f'Total number of companies: {len(volumes)}')
     print(f'Number with full representation: {sum([1 for volume in volumes if len(volumes[volume]) == 364])}')
