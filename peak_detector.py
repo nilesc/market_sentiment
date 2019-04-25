0573bf6e27c694b3726af7c139dd9ddad4301172
Niles Christensen <nnc2117@columbia.edu>
2019-04-25 00:21:09 -0400

    [Adds] an ability to tell how many tweets were made on a given day

--- peak_detector.py
+++ peak_detector.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import sys
 
 if __name__ == '__main__':
@@ -7,5 +8,18 @@ if __name__ == '__main__':
         sys.exit()
 
     tweets_directory = sys.argv[1]
-    for company in os.listdir(tweets_directory):
-        print(company)
+    all_companies = [company for company in os.listdir(tweets_directory)
+                     if os.path.isdir(os.path.join(tweets_directory, company))]
+    for company in all_companies:
+        print(f'Analyzing: {company}')
+        company_dir = os.path.join(tweets_directory, company)
+        all_days = sorted(os.listdir(company_dir))
+        tweet_volume = []
+        for day in all_days:
+            day_path = os.path.join(company_dir, day)
+            with open(day_path, 'r') as f:
+                reader = csv.reader(f)
+                day_volume = sum([1 for row in reader])
+                tweet_volume.append(day_volume)
+        print(tweet_volume)
+        print()
