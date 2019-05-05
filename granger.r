e599c60d6c27fced435563ae27a4ca52d0384475
pstance <pstanton15@gmail.com>
2019-05-05 16:52:53 -0400

    Add files via upload

---
+++ granger.r
@@ -0,0 +1,86 @@
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/AAPL.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/AXP.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/BA.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/CSCO.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/CVX.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/DIS.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/DWDP.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/GS.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/HD.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/IBM.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/INTC.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/JNJ.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/JPM.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/KO.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/MCD.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/MMM.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/MRK.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/MSFT.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/NKE.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/PFE.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/PG.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/TRV.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/UNH.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/UTX.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/V.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/VZ.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/WBA.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/WMT.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
+
+percent_change.sentiment <- read.csv("~/Documents/columbia/EMODS/project/market_sentiment/analysis/XOM.csv", header=FALSE)
+grangertest(percent_change.sentiment$V3,percent_change.sentiment$V2,order=1)
